5c57c6bcef4bb1ac59f4592632244cc3919d75dd
Steven Roland <steven@mojave.io>
2016-04-20 22:43:15 -0400

    Deleting a single user
    - Created DELETE route to remove a single user from the database after
    locating using the user's ID.

--- server.js
+++ server.js
@@ -63,6 +63,15 @@ api.route('/users/:user_id')
                 res.json({ message: 'User updated!' });
             });
         });
+    }).delete(function(req, res) {
+        User.remove({
+            _id: req.params.user_id
+        }, function(err, user) {
+            if (err){
+                res.send(err);
+            }
+            res.json({ message: 'Successfully deleted' });
+        });
     });
 
 app.use('/api', api);
