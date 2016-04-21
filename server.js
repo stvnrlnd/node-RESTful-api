04c776b50f141f2bc76c01cd07fdb175e9b9d280
Steven Roland <steven@mojave.io>
2016-04-20 21:49:16 -0400

    Creating a user
    - Created the POST route to add new users to the database.

--- server.js
+++ server.js
@@ -21,6 +21,18 @@ api.get('/', function(req, res) {
   res.json({ message: 'Hello, beautiful!' });
 });
 
+api.route('/users')
+    .post(function(req, res) {
+        var user = new User();
+        user.name = req.body.name;
+        user.save(function(err) {
+            if (err){
+                res.send(err);
+            }
+            res.json({ message: 'User created!' });
+        });
+    });
+
 app.use('/api', api);
 
 // ------ Serve
